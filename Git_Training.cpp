14da99a7f092ad96ab82c89cc2eaf3b05b138f66
Takayuki Iitsuka <takayuki.iitsuka@gmail.com>
2023-06-11 06:41:50 +0900

    float

--- Git_Training.cpp
+++ Git_Training.cpp
@@ -7,6 +7,9 @@ int main(void)
 {
     std::cout << "Hello World!\n\n";
 
+	const float MY_PI = 3.14159265;
+	float val_rad, val_sin, val_cos;
+
 	for (int i = 0; i < 20; i++)
 	{
 		std::cout << i << std::endl;
